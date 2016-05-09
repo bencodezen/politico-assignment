7f7bf5b2cdb0d09cff2054ea86cb1feb5fb1d2b9
Ben Hong <bencodezen@gmail.com>
2016-05-09 11:21:56 -0400

    Add overlay styles and functionality

--- app/js/index.js
+++ app/js/index.js
@@ -1,5 +1,17 @@
 (function() {
+  // Mobile menu toggle functionality
   $('#toggle-nav').on('click', function() {
     $('#nav-list').slideToggle();
+  });
+
+  // Photo overlay functionality
+  var height = $(document).height();
+
+  $('#overlay').css('height', height);
+  $('.overlay__close').on("click", function() {
+    $("#overlay").fadeOut(200);
+  });
+  $("#toggle-overlay").on("click", function() {
+    $("#overlay").css('display', 'flex').fadeIn();
   });
 })();
